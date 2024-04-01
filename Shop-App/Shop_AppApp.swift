02328dda5eb0ede7106e5be4ee4e7c6ba9fddd3a
“AhmedEl-Hussin” <Ahmad.elhussin03@gmail.com>
2024-04-01 23:28:10 +0200

    Initial Commit

---
+++ Shop-App/Shop_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shop_AppApp.swift
+//  Shop-App
+//
+//  Created by Ahmed Elhussin on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Shop_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
